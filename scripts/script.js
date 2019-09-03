a9421b35de48da257159894997dc2cbaca5afe88
EmilyW <ejwheelock@gmail.com>
2019-09-03 19:52:06 -0400

    add button to change grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,46 @@
-console.log('Hello, world!')
+console.log('Welcome to your Etch-a-Sketch!');
 
-let container = document.querySelector('div#palette');
 
-for (let i=0; i<16; i++) {
-    for (let j=0; j<16; j++) {
-        let grid = document.createElement('div');
-        grid.classList.add('square');
-        grid.style.gridColumnStart = `"${i+1}"`;
-        grid.style.gridColumnEnd = `"${i+2}"`;
-        grid.style.gridRowStart = `"${j+1}`;
-        grid.style.gridRowEnd = `"${j+2}"`;
-        container.appendChild(grid);
+function createGrid(side) {
+    let con = document.querySelector('#palette');
+    while (con.hasChildNodes()) {
+        con.removeChild(con.lastChild);
     }
+    
+    for (let i=0; i<side; i++) {
+        for (let j=0; j<side; j++) {
+            let grid = document.createElement('div');
+            grid.classList.add('square');
+            grid.style.gridColumnStart = `"${i+1}"`;
+            grid.style.gridColumnEnd = `"${i+2}"`;
+            grid.style.gridRowStart = `"${j+1}`;
+            grid.style.gridRowEnd = `"${j+2}"`;
+            grid.addEventListener("mouseenter",changeColor);
+            con.appendChild(grid);
+        }
+    }
+
+    let gridTemplateCols = "";
+    for (let k=0; k<side; k++) {
+        gridTemplateCols += "auto ";
+    }
+    con.style.gridTemplateColumns = gridTemplateCols;
 }
+
+function promptForSize() {
+    let num = prompt("How large a grid number, N, where grid is NxN?");
+    num = parseInt(num);
+    createGrid(num);
+}
+
+function changeColor(e) {
+    e.target.classList.add('colored');
+}
+
+createGrid(16);
+
+let clearButton = document.querySelector('button');
+clearButton.addEventListener("click", promptForSize);
+
+
 
